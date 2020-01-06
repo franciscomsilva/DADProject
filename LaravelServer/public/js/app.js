db89fda05b56574e8712af45322311d1d3d280bc
Airtik <nunomcq@hotmail.com>
2020-01-06 19:05:40 +0000

    sim

--- LaravelServer/public/js/app.js
+++ LaravelServer/public/js/app.js
@@ -26701,7 +26701,7 @@ var render = function() {
                                                 },
                                                 [
                                                   _vm._v(
-                                                    "\n          edit\n        "
+                                                    "\r\n          edit\r\n        "
                                                   )
                                                 ]
                                               )
@@ -26711,7 +26711,7 @@ var render = function() {
                                       ],
                                       null,
                                       false,
-                                      3917885797
+                                      3954612837
                                     )
                                   })
                                 ],
@@ -82448,8 +82448,8 @@ vue__WEBPACK_IMPORTED_MODULE_0___default.a.toasted.register('unauthorized', 'Una
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/fsilva/dev/Projects/DADProject/LaravelServer/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/fsilva/dev/Projects/DADProject/LaravelServer/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Software\laragon\www\DADProject\LaravelServer\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Software\laragon\www\DADProject\LaravelServer\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
