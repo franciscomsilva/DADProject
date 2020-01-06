bcf1882d38b292129b8017581cbab1d1b7c0db2a
Francisco Silva <franciscosilva800@gmail.com>
2020-01-06 18:11:40 +0000

    Adds unknow user photo

--- LaravelServer/resources/views/vue/index.blade.php
+++ LaravelServer/resources/views/vue/index.blade.php
@@ -15,9 +15,9 @@
 	        	<v-list-item>
 	            	<v-list-item-avatar>
 		        		<!-- Guest user avatar -->
-	              		<v-img v-if="!$store.state.user " src="https://www.standingstills.com/pub/media/catalog/product/cache/75eed2686e01eb22cb4050b2f40ddf97/c/a/cad195-1-1.jpg"></v-img>
+	              		<v-img v-if="!$store.state.user || !$store.state.user.photo" src="{{asset('storage/fotos/unknow_user.png')}}"></v-img>
 						<v-img v-else :src="'{{asset('storage/fotos')}}/' +  $store.state.user.photo"/>
-		          	</v-list-item-avatar>	
+		          	</v-list-item-avatar>
 		        </v-list-item>
 		        
 	          	<v-list-item link two-line>
